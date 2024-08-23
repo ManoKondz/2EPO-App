e0bccd1db2ee1c5cdf40925dff5fb3f0a40a636d
OE17 <otavio.lira08@aluno.ifce.edu.br>
2024-08-23 14:51:05 -0300

    Criação da tela do desafio 5 e altercações na tela 3

--- 2EPO/2EPO/L1Desafio5.swift
+++ 2EPO/2EPO/L1Desafio5.swift
@@ -1,9 +1,9 @@
 import SwiftUI
 
-struct Licao3: View {
+struct Licao5: View {
     @State private var showingPopup = false
     @State private var selectedOption = ""
-
+    
     func textForIndex(_ index: Int) -> String {
         switch index {
         case 0:
@@ -18,13 +18,13 @@ struct Licao3: View {
             return ""
         }
     }
-
+    
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.menu // Cor de fundo aplicada a toda a tela
                     .edgesIgnoringSafeArea(.all) // Garante que a cor preencha toda a tela
-
+                
                 VStack {
                     HStack {
                         Button(action: {
@@ -36,34 +36,39 @@ struct Licao3: View {
                         Spacer()
                     }
                     .padding()
-
+                    
                     // Barra de progresso
                     ZStack(alignment: .leading) {
                         RoundedRectangle(cornerRadius: 60)
                             .fill(Color.barcolor)
                             .frame(height: 25) // Define altura da barra de fundo
-
+                        
                         RoundedRectangle(cornerRadius: 60)
                             .fill(Color.progress)
-                            .frame(width: 150, height: 25) // A largura é ajustada com base no progresso
+                            .frame(width: 361, height: 25) // A largura é ajustada com base no progresso
                     }
                     .padding(.horizontal)
-
+                    
                     Text("Após concluir uma série de cartazes para participar do movimento sobre a língua inglesa, o grupo das mãos precisa escolher um `slogan`.")
                         .foregroundColor(.white)
                         .multilineTextAlignment(.center)
                         .padding()
-
+                        .frame(height: 90)
+                        .font(.system(size: 16))
+                        .layoutPriority(1)
                     
                     VStack{
+                        // Retângulo branco
                         RoundedRectangle(cornerRadius: 20)
                             .fill(Color.white)
                             .frame(width: 300, height: 200)
-                            .padding(.vertical, 10)
+                            
+                        
                         Text("Selecione o `slogan` mais apropriado.")
                             .foregroundColor(.white)
                             .padding(.top, 10)
                         
+                        // Caixa verde das respostas
                         VStack(spacing: 0) {
                             ForEach(0..<4, id: \.self) { index in
                                 Button(action: {
@@ -84,36 +89,35 @@ struct Licao3: View {
                                 }
                             }
                         }
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(Color.blue, lineWidth: 2)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .fill(Color.botões)
+                                )
+                        )
+                        .padding(.bottom, 20)
+                        
+                        // Botão de som
+                        Button(action: {
+                            // Ação do botão de som
+                        }) {
+                            Image(systemName: "speaker.wave.2.fill")
+                                .frame(width: 120, height: 50)
+                                .padding()
+                                .background(Color.botãof)
+                                .cornerRadius(10)
+                                .foregroundColor(.white)
+                        }
                     }
-                    .padding(.horizontal)
-                    .background(
-                        RoundedRectangle(cornerRadius: 20)
-                            .stroke(Color.blue, lineWidth: 2)
-                            .background(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(Color.botões)
-                            )
-                    )
-                    .padding(.bottom, 20)
-
-                    // Botão de som
-                    Button(action: {
-                        // Ação do botão de som
-                    }) {
-                        Image(systemName: "speaker.wave.2.fill")
-                            .frame(width: 50, height: 50)
-                            .padding()
-                            .background(Color.botãof)
-                            .cornerRadius(10)
-                            .foregroundColor(.white)
-                    }
+                    .padding()
                 }
-                .padding()
             }
         }
-    }
-}
+    }}
 
 #Preview {
-    Licao3()
+    Licao5()
 }
