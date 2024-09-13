81a280c017281eeb23bbfc84a36e1fd4f938a6f7
ManoKondz <egabrielrbm@gmail.com>
2024-09-13 08:09:03 -0300

    Import e construção da função que configura e inicia a reprodução do áudio junto com a documentação do código para facilitar leitura

--- 2EPO/2EPO/L1Desafio1.swift
+++ 2EPO/2EPO/L1Desafio1.swift
@@ -1,32 +1,46 @@
-import SwiftUI
+// Comentários Powered by XachátGPT
 
+import SwiftUI
+import AVFoundation
+
+// Definição da view "Licao1", que representa a primeira lição.
 struct Licao1: View {
+    // Estados que controlam a exibição do popup, o feedback da resposta e a navegação para a próxima tela.
     @State private var showingResult = false
     @State private var isCorrect = false
     @State private var navigateToNextScreen = false
     
+    // Sintetizador de fala para leitura de texto em voz alta.
+    private let speechSynthesizer = AVSpeechSynthesizer()
+
     var body: some View {
+        // Cria uma pilha de navegação para controlar a transição entre telas.
         NavigationStack {
             ZStack {
+                // Definição de cor de fundo para toda a tela.
                 Color.menu
                     .edgesIgnoringSafeArea(.all)
                 
                 VStack {
+                    // Barra de progresso no topo, com estilo arredondado.
                     ZStack(alignment: .leading) {
                         RoundedRectangle(cornerRadius: 60)
                             .fill(Color.barcolor)
                             .frame(height: 25)
+                        
+                        // Parte preenchida da barra de progresso, indicando 72 unidades de avanço.
                         RoundedRectangle(cornerRadius: 60)
                             .fill(Color.progressBar)
                             .frame(width: 72, height: 25)
                     }
                     .padding(.horizontal)
                     
+                    // Texto que descreve o contexto da lição.
                     Text("Dois grupos estão discutindo qual a forma correta de escrever a palavra \"mão\" no plural")
                         .foregroundStyle(.white)
                         .multilineTextAlignment(.leading)
                     
-                    
+                    // Exibe uma imagem relacionada ao desafio.
                     Image("desafio1")
                         .resizable()
                         .scaledToFit()
@@ -35,39 +49,41 @@ struct Licao1: View {
                         .cornerRadius(60)
                         .foregroundColor(.black)
                     
+                    // Instrução para o usuário interagir com o exercício.
                     Text("Declare apoio ao grupo correto")
                         .foregroundStyle(.white)
                         .multilineTextAlignment(.center)
                         .font(.headline)
                         .padding()
                     
-                    
-                    //Botões de resposta: Mões e Mãos
+                    // Botões de múltipla escolha: "Mões" e "Mãos".
                     HStack(spacing: 20) {
+                        // Botão que o usuário pode selecionar. No caso de seleção incorreta ("Mões").
                         Button(action: {
-                            isCorrect = false
-                            showingResult = true
+                            isCorrect = false // Define a resposta como incorreta.
+                            showingResult = true // Exibe o popup de resultado.
                         }) {
                             VStack {
-                                Image(systemName: "person.3.fill")
+                                Image(systemName: "person.3.fill") // Ícone gráfico
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 60, height: 110)
-                                Text("Mões")
+                                Text("Mões") // Texto da opção.
                                     .font(.headline)
                                     .padding(.bottom, 10)
                             }
                             .frame(maxWidth: .infinity)
                             .padding()
-                            .background(Color.botaoOpcao)
+                            .background(Color.botaoOpcao) // Cor de fundo do botão.
                             .cornerRadius(20)
                             .foregroundColor(.black)
                             .shadow(radius: 5)
                         }
                         
+                        // Botão que o usuário pode selecionar. No caso de seleção correta ("Mãos").
                         Button(action: {
-                            isCorrect = true
-                            showingResult = true
+                            isCorrect = true // Define a resposta como correta.
+                            showingResult = true // Exibe o popup de resultado.
                         }) {
                             VStack {
                                 Image(systemName: "person.3.fill")
@@ -88,10 +104,11 @@ struct Licao1: View {
                     }
                     .padding(.horizontal)
                     
+                    // Botão de som para ativar a leitura em voz alta (função a ser implementada).
                     Button(action: {
-                        // Ação do botão de som
+                        // Ação do botão de som (não implementada neste exemplo).
                     }) {
-                        Image(systemName: "speaker.3.fill")
+                        Image(systemName: "speaker.3.fill") // Ícone de som.
                             .frame(width: 100)
                             .padding()
                             .background(Color.botaoPadrao)
@@ -101,36 +118,49 @@ struct Licao1: View {
                     .padding(.top, 20)
                 }
                 .padding()
+                
+                // Exibe o popup de feedback ao usuário quando a resposta é dada.
                 .overlay(
                     CustomPopupView(isCorrect: isCorrect, showing: $showingResult, navigateToNextScreen: $navigateToNextScreen)
                 )
                 
-                NavigationLink(value: navigateToNextScreen)
-                    {
+                // Link de navegação que leva à próxima lição (Licao2).
+                NavigationLink(value: navigateToNextScreen) {
                     EmptyView()
                 }
-                    .navigationDestination(isPresented:$navigateToNextScreen){
-                        Licao2()
-                    }
+                .navigationDestination(isPresented: $navigateToNextScreen) {
+                    Licao2() // A próxima tela que será exibida.
+                }
             }
         }
     }
+    
+    // Função que usa o sintetizador de fala para ler um texto em voz alta.
+    func speakText(_ text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "pt-BR") // Define o idioma para português.
+        utterance.rate = 0.5 // Ajusta a velocidade da fala.
+        speechSynthesizer.speak(utterance) // Inicia a fala.
+    }
 }
 
+// Popup customizado que exibe o feedback ao usuário após responder a questão.
 struct CustomPopupView: View {
-    var isCorrect: Bool
-    @Binding var showing: Bool
-    @Binding var navigateToNextScreen: Bool
+    var isCorrect: Bool // Indica se a resposta do usuário está correta.
+    @Binding var showing: Bool // Controla a exibição do popup.
+    @Binding var navigateToNextScreen: Bool // Controla a navegação para a próxima tela.
     
     var body: some View {
         if showing {
             VStack {
                 HStack {
+                    // Ícone que indica se a resposta está certa ou errada.
                     Image(systemName: isCorrect ? "checkmark.circle.fill" : "exclamationmark.triangle.fill")
                         .resizable()
                         .frame(width: 30, height: 30)
                         .foregroundColor(.white)
                     
+                    // Mensagem de feedback com base na resposta do usuário.
                     VStack(alignment: .leading) {
                         Text(isCorrect ? "Excelente! Parabéns!" : "Ops... Na próxima dá certo")
                             .foregroundColor(.white)
@@ -139,6 +169,7 @@ struct CustomPopupView: View {
                     
                     Spacer()
                     
+                    // Ícone de som (potencial para leitura em voz alta do feedback).
                     Image(systemName: "speaker.wave.3.fill")
                         .resizable()
                         .frame(width: 20, height: 20)
@@ -149,9 +180,10 @@ struct CustomPopupView: View {
                 .cornerRadius(15)
                 .padding()
                 
+                // Botão para avançar para a próxima tela.
                 Button(action: {
-                    showing = false
-                    navigateToNextScreen = true // Ativar navegação para a próxima tela
+                    showing = false // Oculta o popup.
+                    navigateToNextScreen = true // Navega para a próxima tela.
                 }) {
                     Image(systemName: "forward.fill")
                         .resizable()
@@ -164,14 +196,13 @@ struct CustomPopupView: View {
                 .cornerRadius(15)
                 .padding(.horizontal, 50)
             }
-            .transition(.move(edge: .bottom))
-            .animation(.easeInOut, value: showing)
+            .transition(.move(edge: .bottom)) // Animação de transição para o popup.
+            .animation(.easeInOut, value: showing) // Animação de aparecimento/desaparecimento do popup.
         }
     }
 }
 
-
-
+// Função de preview no Xcode.
 #Preview {
     Licao1()
 }
